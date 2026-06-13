4ba31b82780b0d0a894b12939dba8cd32ef63de4
olorusobuur1-boop <olorusobuur1@gmail.com>
2026-06-13 07:01:51 +0100

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const navbar = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit nihil
+        doloribus, animi, optio sint hic, non voluptas suscipit perferendis odio
+        ut blanditiis necessitatibus ullam dolorem minima itaque nesciunt
+        reiciendis commodi?
+      </p>
+    </div>
+  );
+};
+
+export default navbar;
